411a203a11257aca682597094740c0b23b2e951b
Yuri <165918527+YurVecchiat@users.noreply.github.com>
2025-08-14 16:45:16 +0200

    Create cases.sql

---
+++ database/csv/cases.sql
@@ -0,0 +1,9 @@
+create or replace TABLE SUMUP.CSV.CASES (
+	CASE_ID VARCHAR(16777216), -- A unique identifier for the customer support case.
+	AGENT VARCHAR(16777216), -- The name of the customer support agent.
+	MANAGER VARCHAR(16777216), -- The name of the manager who oversees the agent.
+	ESCALATED_AT TIMESTAMP_NTZ(9), -- The date and time when the case was escalated to the elite squad
+team.
+	FIRST_REPLY_AT TIMESTAMP_NTZ(9) -- The date and time when the elite squad team first replied to the
+case.
+);
