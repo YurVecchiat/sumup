8021c1bcd46e04fa47191f821aabae500d8ac7dc
Yuri <165918527+YurVecchiat@users.noreply.github.com>
2025-08-14 16:43:54 +0200

    Create schedules.sql

---
+++ database/csv/schedules.sql
@@ -0,0 +1,6 @@
+create or replace TABLE SUMUP.CSV.SCHEDULES (
+	AGENT VARCHAR(16777216), -- The name of the customer support agent.
+	CHANNEL VARCHAR(16777216), -- The communication channel used by the customer to contact us.
+	SCHEDULE_DATE DATE, -- The date on which a task or event is scheduled to occur.
+	SCHEDULED_HOURS NUMBER(38,0) -- The number of hours allocated for a particular task or event.
+);
