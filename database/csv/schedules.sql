778087704d9fc9cf5070adcb374b3f522e0eec2c
Yuri <165918527+YurVecchiat@users.noreply.github.com>
2025-08-15 11:15:05 +0200

    Update schedules.sql

--- database/csv/schedules.sql
+++ database/csv/schedules.sql
@@ -4,5 +4,5 @@ create or replace TABLE SUMUP.CSV.SCHEDULES (
 	SCHEDULE_DATE DATE, -- The date on which a task or event is scheduled to occur.
 	SCHEDULED_HOURS NUMBER(38,0) -- The number of hours allocated for a particular task or event.
 )
-	comment = 'Table contains containing information about the productive hours worked bySumUp's customer support agents.'
+	comment = 'Table contains containing information about the productive hours worked bySumUps customer support agents.'
 ;
