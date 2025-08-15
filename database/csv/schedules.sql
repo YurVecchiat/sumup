aa1a83696968ee3ff1d4bce0308cf4e78560dd87
Yuri <165918527+YurVecchiat@users.noreply.github.com>
2025-08-15 09:03:36 +0200

    Update schedules.sql

--- database/csv/schedules.sql
+++ database/csv/schedules.sql
@@ -3,4 +3,6 @@ create or replace TABLE SUMUP.CSV.SCHEDULES (
 	CHANNEL VARCHAR(16777216), -- The communication channel used by the customer to contact us.
 	SCHEDULE_DATE DATE, -- The date on which a task or event is scheduled to occur.
 	SCHEDULED_HOURS NUMBER(38,0) -- The number of hours allocated for a particular task or event.
-);
+)
+	comment = 'Table contains containing information about the productive hours worked bySumUp's customer support agents.'
+;
