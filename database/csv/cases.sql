4a9e6a5fb1f74e868137b2c08395b4e55aa2baa3
Yuri <165918527+YurVecchiat@users.noreply.github.com>
2025-08-15 09:02:26 +0200

    Update cases.sql

--- database/csv/cases.sql
+++ database/csv/cases.sql
@@ -4,6 +4,7 @@ create or replace TABLE SUMUP.CSV.CASES (
 	MANAGER VARCHAR(16777216), -- The name of the manager who oversees the agent.
 	ESCALATED_AT TIMESTAMP_NTZ(9), -- The date and time when the case was escalated to the elite squad
 team.
-	FIRST_REPLY_AT TIMESTAMP_NTZ(9) -- The date and time when the elite squad team first replied to the
-case.
+	FIRST_REPLY_AT TIMESTAMP_NTZ(9) -- The date and time when the elite squad team first replied to the case
+	comment = 'Table containing information about cases that were escalated to SumUp's elite squad team.'
+;
 );
